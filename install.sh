5110bca641408edc727816bc28983c1e9e948c6c
Rigoberto Ortega <genesis-os@hotmail.com>
2024-10-26 23:25:07 -0600

    First commit

---
+++ install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+################################################################################
+##  █████╗ ██████╗  ██████╗██╗  ██╗    ██╗     ██╗███╗   ██╗██╗   ██╗██╗  ██╗ ##
+## ██╔══██╗██╔══██╗██╔════╝██║  ██║    ██║     ██║████╗  ██║██║   ██║╚██╗██╔╝ ##
+## ███████║██████╔╝██║     ███████║    ██║     ██║██╔██╗ ██║██║   ██║ ╚███╔╝  ##
+## ██╔══██║██╔══██╗██║     ██╔══██║    ██║     ██║██║╚██╗██║██║   ██║ ██╔██╗  ##
+## ██║  ██║██║  ██║╚██████╗██║  ██║    ███████╗██║██║ ╚████║╚██████╔╝██╔╝ ██╗ ##
+## ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝    ╚══════╝╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚═╝  ╚═╝ ##
+##                                                                            ##
+## Author: gen3sis0s                                                          ##
+## Github: https://github.com/gen3sis0s                                       ##
+################################################################################
+
+
+
+# Set scripts directory
+SCRIPT_DIRECTORY="$(dirname "$(realpath "$0")")"
+
+# Load common functions and variables
+source "$SCRIPT_DIRECTORY/scripts/common.sh"
+
+# Check if the script is run as root
+if [[ $EUID -ne 0 ]]; then
+    log_message ERROR "This script must be run as root."
+    exit 1
+fi
+
+logotype
+
+log_message INFO "Starting Arch Linux Installation Script"
+
+
+# Call sub-scripts
+
+
+
+log_message INFO "Installation completed successfully."
+
+
+
+
+
